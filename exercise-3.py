facf5b9e349be87cc90a1204ff79e3997f7d2a7a
Armando Samperio <armandosamperiomcchs@Armandos-MacBook-Pro.local>
2019-09-04 13:38:11 -0500

    finish problem 4

--- exercise-3.py
+++ exercise-3.py
@@ -2,14 +2,14 @@
 
 # Write the code that:
 # 1. Prompts the user to enter a dog's age in human years like this:
-#      Input a dog's age in human years: 
+#      Input a dog's age in human years:
 # 2. Calculates the equivalent dog years, where:
 #      - The first two years count as 10 years each
 #      - Any remaining years count as 7 years each
 # 3. Prints the answer in the following format:
 #      The dog's age in dog years is xx
 
-# Hint:  Use the int() function to convert the string returned from input() into an integer
+# Hint:  Use the int() function to convert the string returned from input()
 
 x = int(input('Enter a dogs age: '))
 dog_years = 0
@@ -20,6 +20,6 @@ for num in range(1, x + 1):
         dog_years += 10
     elif num == 2:
         dog_years += 10
-    else: 
+    else:
         dog_years += 7
 print('The dogs age in dog years is ' + str(dog_years))
--- exercise-4.py
+++ exercise-4.py
@@ -3,7 +3,7 @@
 # Write the code that:
 # 1. Prompts the user to enter the three lengths of a triangle (one at a time):
 #      Enter the lengths of three side of a triangle:
-#      a: 
+#      a:
 #      b:
 #      c:
 # 2. Write the code that determines if the triangle is:
@@ -11,4 +11,15 @@
 #      scalene - all three sides are unequal in length
 #      isosceles - two sides are the same length
 # 3. Print a message such as:
-#      - A triangle with sides of <a>, <b> & <c> is a <type of triangle> triangle
+#      - A triangle with sides of <a>, <b> & <c> is a <type of triangle>
+
+a = input('Enter first length of the triangle: ')
+b = input('Enter second length of the triangle: ')
+c = input('Enter third length of the triangle: ')
+
+if a == b and a == c:
+    print(f'A triangle with sides of {a}, {b}, {c} is a equalateral of triangle')
+elif a == b or a == c:
+    print(f'A triangle with sides of {a}, {b}, {c} is a isosceles of triangle')
+else:
+    print(f'A triangle with sides of {a}, {b}, {c} is a scalene triangle')
